db5595bad2be6affab91739105070ac9b23a7716
Taiga Miwa <taiga.miwa888@gmail.com>
2021-04-04 20:05:00 +0900

    roomを作れるようになった

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { useQuery, gql } from '@apollo/client';
 import './App.css';
 import TopPage from './TopPage'
-
-const EXCHANGE_RATES = gql`
-  query GetExchangeRates {
-    rates(currency: "USD") {
-      currency
-      rate
-    }
-  }
-`;
 
 const App = () => {
   return (
--- src/TopPage.tsx
+++ src/TopPage.tsx
@@ -1,6 +1,6 @@
 import kusorepMan from './images/kusorep_man.png'
 import './App.css';
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, useMutation, gql } from '@apollo/client';
 
 const ROOMS = gql`
   query {
@@ -12,8 +12,19 @@ const ROOMS = gql`
   }
 `;
 
+const NEW_ROOM = gql`
+  mutation {
+    newRoom(input: {}) {
+      room {
+        id
+      }
+    }
+  }
+`;
+
 const TopPage = () => {
   const { loading, error, data } = useQuery(ROOMS);
+  const [newRoom, { data: mutationData }] = useMutation(NEW_ROOM);
   console.log(data)
   return (
     <>
@@ -21,7 +32,15 @@ const TopPage = () => {
         <h1>クソリプメーカー</h1>
         <p>あなたも品性のあるクソリプを</p>
       </div>
-      <button className="btn btn-blue btn-lg">ルームを作成する</button>
+      <button
+        className="btn btn-blue btn-lg"
+        onClick={(e) => {
+          e.preventDefault();
+          newRoom()
+        }}
+      >
+        ルームを作成する
+      </button>
       <div><img src={kusorepMan} alt="クソリプマン"/></div>
     </>
   );
